7bc8ee0be113d6bbda70915be3af6e3d63693f82
Allan Pooley <allanpooley@outlook.com>
2017-05-29 16:12:40 +1000

    Set up project directory for React front-end development

---
+++ README.md
@@ -0,0 +1,10 @@
+# Challenges!
+
+README to be authored!
+
+To begin, run:
+
+npm install
+npm start
+
+App will run on http://localhost:8080
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+	// map state to reducers here
+});
+
+export default rootReducer;
